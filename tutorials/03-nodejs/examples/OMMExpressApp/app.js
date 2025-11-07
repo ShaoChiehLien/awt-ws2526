6a044e38bcc34f6b73478f8046878cec44b45e20
Shao-Chieh Lien <lienshaochieh@gmail.com>
2025-11-07 22:03:38 +0100

    03-nodejs: add new route of product
    - add get route to allow users to query different products
    - add put route to allow users to add new product

--- tutorials/03-nodejs/examples/OMMExpressApp/app.js
+++ tutorials/03-nodejs/examples/OMMExpressApp/app.js
@@ -6,24 +6,32 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
 
 var app = express();
 
-// view engine setup
+// view engine setup. A key value pair for configuring express
+// e.g. view engine is key, jade is value
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// unlike get, app.use handles all requests
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// if no send and next called in router (middleware), it will simply hang there
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); // Express automatically ignores trailing slashes
+// Paths that would match: /products, /products/, /products/electric_products, /products/electric_products/shaver
+app.use('/products', productsRouter); // the path is better to have the same name as the filename in routes
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  // with error parameter, it skips remaining non-error handling middlewares
+  // and goes to error handling middlewares
   next(createError(404));
 });
 
